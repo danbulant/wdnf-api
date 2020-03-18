39bfcb050b2c984a0fda71ef56779454db791fc5
Daniel Bulant <admin@danbulant.eu>
2020-03-18 16:32:31 +0100

    ✨Add error handling

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.get("/", (req, res)=>{
 app.use(morgan("tiny"));
 app.use("/v1", v1);
 
+app.use((req, res, next, err)=>{
+    console.error(err);
+    res.code(500).json({
+        type: "e_internal_error"
+    })
+})
+
 app.listen(14453, ()=>{
     console.log("Ready!");
 });
