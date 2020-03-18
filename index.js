0125d5eceb80b3b152938f5c6421d498f81e4485
Daniel Bulant <admin@danbulant.eu>
2020-03-18 16:25:27 +0100

    ✨Add logger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 const v1 = require("./v1");
@@ -10,6 +11,8 @@ app.get("/", (req, res)=>{
     })
 })
 
+
+app.use(morgan());
 app.use("/v1", v1);
 
 app.listen(14453, ()=>{
